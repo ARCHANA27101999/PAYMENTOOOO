89aebc175e9ae933f1a9bb10ce8729c8f9df9abb
k p archana <kparchana2@gmail.com>
2022-11-06 23:47:45 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LOginn from './PAges/Auth/LOginn';
+
+function App() {
+  return (
+    <div >
+      <LOginn/>
+    </div>
+  );
+}
+
+export default App;
